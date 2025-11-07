a23ea5c566e541b4f6179b076b977f511584508a
Wallace Santos <wallace.santos@ibar.com.br>
2025-11-07 08:17:05 -0300

    desafio vc a corrijir os problemas deste codigo

--- app/main.py
+++ app/main.py
@@ -8,5 +8,24 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/cadastro_produto', methods=['GET'])
+def cadastro_produto():
+    return render_template('cadastro_produto.html')
+
+@app.route('/entrada_produto', methods=['GET'])
+def entrada_produto():
+    return render_template('entrada_produto.html')
+
+
+@app.route('/saida_produto', methods=['GET'])
+def saida_produto():
+    return render_template('saida_produto.html')
+
+
+@app.route('/relatorios', methods=['GET'])
+def relatorios():
+    return render_template('relatorios.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, port='5001')
