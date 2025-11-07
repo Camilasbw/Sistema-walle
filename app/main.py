eacf66e37c824ce97b4faa0adb73ebaf7734504b
Wallace Santos <wallace.santos@ibar.com.br>
2025-11-07 07:58:56 -0300

    camila nao faz 1 commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='templates/static')
 
 @app.route('/', methods=['GET'])
 def index():
@@ -9,4 +9,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port='5001')
